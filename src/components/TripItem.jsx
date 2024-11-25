42b142c80c32845f4976088ddc2db0dfe88ece21
hoken92 <hkenneth92@gmail.com>
2024-11-24 20:52:50 -0800

    added more styling to trips page

--- src/components/TripItem.jsx
+++ src/components/TripItem.jsx
@@ -8,6 +8,8 @@ export default function TripItem({ trip, handleEdit, handleDelete }) {
         <h4>Departing Flight</h4>
         <h4>Returning Flight</h4>
         <h4>
+          Date:
+          <br />
           {format(trip.event_info.start_date, "yyyy/MM/dd")} -{" "}
           {format(trip.event_info.end_date, "yyyy/MM/dd")}
         </h4>
@@ -17,17 +19,29 @@ export default function TripItem({ trip, handleEdit, handleDelete }) {
       </div>
       <div className="tripBody">
         <div>
-          <p>{trip.event_info.location}</p>
-          <p>{format(trip.event_info.start_date, "yyyy/MM/dd p")}</p>
-          <p>{format(trip.event_info.end_date, "yyyy/MM/dd p")}</p>
+          <p>
+            <b>City: </b> {trip.event_info.location}
+          </p>
+          <p>
+            <b>Start: </b> {format(trip.event_info.start_date, "yyyy/MM/dd p")}
+          </p>
+          <p>
+            <b>End: </b> {format(trip.event_info.end_date, "yyyy/MM/dd p")}
+          </p>
         </div>
         <div>
           <p>
             {trip.depart_flight_info.origin} to{" "}
             {trip.depart_flight_info.destination}
           </p>
-          <p>{format(trip.depart_flight_info.departureDate, "yyyy/MM/dd p")}</p>
-          <p>{format(trip.depart_flight_info.arriveDate, "yyyy/MM/dd p")}</p>
+          <p>
+            <b>Depart: </b>
+            {format(trip.depart_flight_info.departureDate, "yyyy/MM/dd p")}
+          </p>
+          <p>
+            <b>Arrival: </b>
+            {format(trip.depart_flight_info.arriveDate, "yyyy/MM/dd p")}
+          </p>
           <p>{trip.depart_flight_info.duration}</p>
         </div>
         <div>
@@ -35,12 +49,21 @@ export default function TripItem({ trip, handleEdit, handleDelete }) {
             {trip.return_flight_info.origin} to{" "}
             {trip.return_flight_info.destination}
           </p>
-          <p>{format(trip.return_flight_info.departureDate, "yyyy/MM/dd p")}</p>
-          <p>{format(trip.return_flight_info.arriveDate, "yyyy/MM/dd p")}</p>
+          <p>
+            <b>Depart: </b>
+            {format(trip.return_flight_info.departureDate, "yyyy/MM/dd p")}
+          </p>
+          <p>
+            <b>Arrival: </b>
+            {format(trip.return_flight_info.arriveDate, "yyyy/MM/dd p")}
+          </p>
           <p>{trip.return_flight_info.duration}</p>
-          <p>{trip.return_flight_info.price}</p>
         </div>
         <div>
+          <h5>Price: </h5>
+          <p>{trip.return_flight_info.price}</p>
+        </div>
+        <div className="btn-container">
           <button className="delete-btn" onClick={() => handleDelete(trip)}>
             Cancel Trip
           </button>
